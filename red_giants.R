e52ffb0cb9187ead89daa3691dc39401cb7340eb
Madeline Huberth <madelinehuberth@gmail.com>
2014-01-12 09:19:27 -0800

    added correlation visual

--- red_giants.R
+++ red_giants.R
@@ -27,7 +27,32 @@ plot(rg8$TIME, rg8$SAP_FLUX, main = "Light Curve red_giant_001027337-20091311051
 rg1_numeric = rg1[sapply(rg1, is.numeric)]
 rg1_matrix = as.matrix(rg1_numeric)
 
+rg2_numeric = rg2[sapply(rg2, is.numeric)]
+rg2_matrix = as.matrix(rg2_numeric)
+
+rg3_numeric = rg3[sapply(rg3, is.numeric)]
+rg3_matrix = as.matrix(rg3_numeric)
+
+rg4_numeric = rg4[sapply(rg4, is.numeric)]
+rg4_matrix = as.matrix(rg4_numeric)
+
 library(Hmisc)
 #correlation matrix of seven numeric variables with p-values
 rcorr(rg1_matrix, type="spearman")
 
+library(corrgram)
+
+corrgram(rg1_matrix, order=NULL, lower.panel=panel.shade,
+         upper.panel=NULL, text.panel=panel.txt,main="Red Giant Data - red_giant_000892010-2009131105131.csv")
+
+
+corrgram(rg1_matrix, order=NULL, lower.panel=panel.shade,
+         upper.panel=NULL, text.panel=panel.txt,main="Red Giant Data - red_giant_000892107_2009131105131.csv")
+
+
+corrgram(rg1_matrix, order=NULL, lower.panel=panel.shade,
+         upper.panel=NULL, text.panel=panel.txt,main="Red Giant Data - red_giant_000892738_2009131105131.csv")
+
+
+corrgram(rg1_matrix, order=NULL, lower.panel=panel.shade,
+         upper.panel=NULL, text.panel=panel.txt,main="Red Giant Data - red_giant_000892760_2009131105131.csv")
